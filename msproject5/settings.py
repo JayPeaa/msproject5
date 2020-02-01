9afa2bb6bbb4f84e018b6b7155afb87a13255726
John Hay <john.paul.hay@outlook.com>
2020-02-01 23:19:29 +0000

    work on base.html, header top and flexbox class integration

--- msproject5/settings.py
+++ msproject5/settings.py
@@ -143,9 +143,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
