bcbfc57e659b17c93de24308e614400008f06b05
John Hay <john.paul.hay@outlook.com>
2020-02-24 20:26:51 +0000

    modified models.py in products and added self def to return category name and product name

--- products/models.py
+++ products/models.py
@@ -2,21 +2,28 @@ from django.db import models
 
 # Create your models here.
 
-class ProductCategories(models.Model):
+class ProductCategory(models.Model):
     category_id = models.Field.primary_key=True
-    category_name = models.TextField(max_length=300)
+    category_name = models.TextField(max_length=30)
+    def __str__(self):
+        return self.category_name
+
+
 
 class Product(models.Model):
     product_id = models.Field.primary_key=True
     product_name = models.CharField(max_length=100)
     product_price = models.DecimalField(max_digits=10, decimal_places=2)
     product_weight = models.DecimalField(max_digits=10, decimal_places=2)
+    product_serves = models.IntegerField(blank=False)
     product_description = models.TextField(max_length=300)
     product_image = models.ImageField(upload_to="static/img", blank=True, null=True)
-    product_category_id = models.ForeignKey(ProductCategories, on_delete=models.CASCADE) #check this but think it's correct
+    product_category_id = models.ForeignKey(ProductCategory, on_delete=models.CASCADE) #check this but think it's correct
     product_update_date = models.DateTimeField(auto_now_add=True)
     product_stock_qty = models.IntegerField(blank=False)
     product_live = models.BooleanField(default=True)
+    def __str__(self):
+        return self.product_name
 
 
 
