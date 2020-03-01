5cff8022d46d30cefb5b56e9831a76427d10fae4
JayPeaa <john.paul.hay@outlook.com>
2020-03-01 11:39:25 +0000

    Worked on products view and product cards. Templating the HTML and rendering context from models

--- products/views.py
+++ products/views.py
@@ -4,10 +4,8 @@ from .models import Product
 # Create your views here.
 # This is the product listing page not the detail view 
 
-def products_view(request, *args, **kwargs):
-    obj= Product.objects.get(id=1)
-    context ={
-       'object': obj
-    }
-    return render(request, "products.html", context)
+def products_view(request):
+    products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
+
 
