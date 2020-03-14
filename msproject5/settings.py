59c8b668ffd6e19e8571799b6337f9cdaa8901b9
JayPeaa <john.paul.hay@outlook.com>
2020-03-14 08:04:11 +0000

    Additional work on checkout

--- msproject5/settings.py
+++ msproject5/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     'users',
     'search',
     'orders',
-    'cart'
+    'cart',
+    'stripe'
 
 ]
 
--- orders/forms.py
+++ orders/forms.py
@@ -15,3 +15,19 @@ class MakePaymentForm(forms.Form):
     expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = User #Order
+        fields = (
+            'full_name', 'last_name'
+        )
+
+    class Meta:
+        model = Profile #Order
+        fields = (
+            'user_phone_number', 'user_country', 'user_postcode',
+            'user_city', 'user_street_address_1', 'user_street_address_2',
+            'user_county'
+        )
