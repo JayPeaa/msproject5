30e245712eed40725ead230a94ed21b78d43b599
JayPeaa <john.paul.hay@outlook.com>
2020-03-21 18:44:04 +0000

    Work on registration form fields, change order and all fields made manditory.

--- users/forms.py
+++ users/forms.py
@@ -11,7 +11,21 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].required=True
+        self.fields['last_name'].required=True
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ('user',)
+        labels = {
+            'user_phone_number' : 'Phone Number',
+            'user_city' : 'City',
+            'user_street_address_1' : 'Address Line 1',
+            'user_street_address_2' : 'Addres Line 2',
+            'user_county' : 'County',
+            'user_country' : 'Country',
+            'user_postcode' : 'Postal Code'
+        }
--- users/models.py
+++ users/models.py
@@ -7,13 +7,13 @@ from django.dispatch import receiver
 class Profile(models.Model):
     """ document here """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_phone_number = models.CharField(max_length=20, blank=False)
-    user_city = models.CharField(max_length=40, blank=False)
     user_street_address_1 = models.CharField(max_length=60, blank=False)
     user_street_address_2 = models.CharField(max_length=60, blank=False)
+    user_city = models.CharField(max_length=40, blank=False)
     user_county = models.CharField(max_length=40, blank=False)
     user_country = models.CharField(max_length=40, blank=False)
-    user_postcode = models.CharField(max_length=20, blank=True)
+    user_postcode = models.CharField(max_length=20, blank=False)
+    user_phone_number = models.CharField(max_length=20, blank=False)
     def __str__(self):
         return self.user.username
 
